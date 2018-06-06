2c5874d0c3d83f5701af8f1053f1e886352898ac
Carlos Martinez <carlosmart626@gmail.com>
2018-06-05 21:05:23 -0500

    Update tests

--- tests/test_dj_blog.py
+++ tests/test_dj_blog.py
@@ -3,23 +3,3 @@
 
 """Tests for `djcms_blog` package."""
 
-import pytest
-
-
-from djcms_blog import djcms_blog
-
-
-@pytest.fixture
-def response():
-    """Sample pytest fixture.
-
-    See more at: http://doc.pytest.org/en/latest/fixture.html
-    """
-    # import requests
-    # return requests.get('https://github.com/audreyr/cookiecutter-pypackage')
-
-
-def test_content(response):
-    """Sample pytest test function with the pytest fixture as an argument."""
-    # from bs4 import BeautifulSoup
-    # assert 'GitHub' in BeautifulSoup(response.content).title.string
