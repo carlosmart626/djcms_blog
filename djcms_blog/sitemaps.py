88137da8c4ed99539fcf84e82b859d435cf5a467
Carlos Martinez <carlosmart626@gmail.com>
2018-09-08 21:53:06 -0500

    Added lastmod property

--- djcms_blog/sitemaps.py
+++ djcms_blog/sitemaps.py
@@ -9,3 +9,6 @@ class PostsSitemap(Sitemap):
 
     def items(self):
         return PostTitle.objects.filter(publisher_is_draft=False, published=True)
+
+    def lastmod(self, obj):
+        return obj.published_date
